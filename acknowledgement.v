db9710591d88ca020de7f38e33a8e4c696f5d5bf
XenialDan <thebigsmilexd@gmail.com>
2021-04-13 03:07:35 +0200

    Simplified ack records code

--- acknowledgement.v
+++ acknowledgement.v
@@ -209,7 +209,8 @@ pub fn (mut r Ack) encode() ByteBuffer {
 
 	// b.put_byte(r.get_id())
 	// if r is Ack {
-	b.put_byte(flag_datagram_ack)
+	//b.put_byte(flag_datagram_nack)
+	b.put_byte(bitflag_ack | bitflag_datagram)
 
 	//}
 	// else if r is Nack {
@@ -224,52 +225,50 @@ pub fn (mut r Ack) encode() ByteBuffer {
 	}
 
 	r.packets.sort(a < b)
+	
+	//the code below splits the packets into [n,n+1..] groups
+	mut buf := r.packets.clone()
+	mut pointer := 0
+	mut parts := [][]int{}
+
+	for {
+		if buf.len <= 2 {
+			//fill with leftover buffer
+			parts << buf
+			break
+		}
+		pointer++
+		//as long as linear
+		if buf[pointer] - buf[pointer-1] == 1 {
+			continue
+		}
+		//extract slice
+		parts << buf[..pointer]
+		buf = buf[pointer..]
+		//reset pointer
+		pointer = 0
+	}
 
 	mut stream := new_bytebuffer([]byte{len: default_buffer_size}) // TODO without len
-	mut pointer := 1
-	mut first_packet := r.packets[0]
-	mut last_packet := r.packets[0]
-
-	mut interval_count := 1
-
-	for pointer < packet_count {
-		mut current_packet := r.packets[pointer]
-		mut difference := current_packet - last_packet
-
-		if difference == 1 {
-			last_packet = current_packet
-		} else {
-			if first_packet == last_packet {
-				stream.put_byte(1)
+	
+	for sets in parts{
+		first_packet := sets[0]
+		last_packet := sets[sets.len]
+		if sets.len == 1{
+			println('ack single $last_packet')
+				stream.put_byte(1)//single packet byte
 				stream.put_ltriad(last_packet)
-				current_packet = last_packet
 			} else {
-				stream.put_byte(0)
+			println('ack range $first_packet $last_packet')
+				stream.put_byte(0)//packet range byte
 				stream.put_ltriad(first_packet)
 				stream.put_ltriad(last_packet)
-
-				last_packet = current_packet
-				first_packet = last_packet
 			}
-			interval_count++
 		}
-
-		pointer++
 	}
 
-	if first_packet == last_packet {
-		stream.put_byte(1)
-		stream.put_ltriad(first_packet)
-	} else {
-		stream.put_byte(0)
-		stream.put_ltriad(first_packet)
-		stream.put_ltriad(last_packet)
-	}
-
-	b.put_short(i16(interval_count))
-
-	// b.put_bytes(stream.buffer)
 	stream.trim()
+	b.put_short(i16(parts.len))
 	b.put_bytes(stream.buffer)
 	return b
 }
